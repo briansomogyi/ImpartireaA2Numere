5270303a772be706ab85b0da37addac70c7b50c5
briansomogyi <brian.somogyi@emanuel.ro>
2023-10-12 10:07:49 +0300

    Done some changes

---
+++ ImpartireaA2Numere/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ImpartireaA2Numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("a="); a=int.Parse(Console.ReadLine());
+            Console.Write("b="); b=int.Parse(Console.ReadLine()); 
+            if (b == 0) 
+            {
+                Console.WriteLine("Impartirea nu poate fie efectuata.");
+            }
+            else 
+            {
+                if (a % b == 0) 
+                {
+                    Console.WriteLine(a / b);
+                }
+                else
+                {
+                    Console.WriteLine("Impartirea nu poate fi efectuata.");
+                }
+            }
+        }
+    }
+}
